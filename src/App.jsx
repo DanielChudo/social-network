91e77aeb9627fc79bde5f58bff12dee5303f9365
Daniel <leinpavel@rambler.ru>
2021-07-24 01:10:39 +0300

    Replace appReducer by local state

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Redirect } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
-import { initializeApp } from './redux/appReducer';
 import { Loader, NavBar } from './components';
 import './App.css';
 import { AuthPage, DialogsPage, ProfilePage, UsersPage } from './pages';
+import { requestAuthUserData } from './redux/authReducer';
 
 function App() {
+  const [ready, setReady] = useState(false);
   const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(initializeApp());
+  useEffect(async () => {
+    dispatch(requestAuthUserData(setReady));
   }, [dispatch]);
 
   const id = useSelector((state) => state.auth.id);
   const isAuth = useSelector((state) => state.auth.isAuth);
-  const initialized = useSelector((state) => state.app.initialized);
-  if (!initialized) {
+
+  if (!ready) {
     return <Loader />;
   }
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,14 +4,12 @@ import profileReducer from './profileReducer';
 import dialogReducer from './dialogReducer';
 import usersReducer from './usersReducer';
 import authReducer from './authReducer';
-import appReducer from './appReducer';
 
 const reducers = combineReducers({
   profilePage: profileReducer,
   dialogPage: dialogReducer,
   usersPage: usersReducer,
   auth: authReducer,
-  app: appReducer,
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
