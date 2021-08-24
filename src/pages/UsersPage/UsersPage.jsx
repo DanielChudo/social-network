2e7e458e5f071cf060d7994d6f8301b9648a4994
Daniel <leinpavel@rambler.ru>
2021-08-24 18:56:46 +0300

    Transfer the functionality from UsersContainer to UsersPage

--- src/pages/UsersPage/UsersPage.jsx
+++ src/pages/UsersPage/UsersPage.jsx
@@ -1,31 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect, useParams } from 'react-router-dom';
 import { User, Paginator, Loader } from '../../components';
+import { follow, requestUsers, unfollow } from '../../redux/usersReducer';
 import './UsersPage.css';
 
-function UsersPage(props) {
-  const {
-    curPage,
-    totalUsers,
-    pageSize,
-    isFetching,
-    followingInProgress,
-    follow,
-    unfollow,
-  } = props;
-  let { users } = props;
-  users = users.map((user) => (
-    <User
-      key={user.id}
-      id={user.id}
-      avatar={user.photos.small}
-      name={user.name}
-      status={user.status}
-      followed={user.followed}
-      follow={follow}
-      unfollow={unfollow}
-      followingInProgress={followingInProgress}
-    />
-  ));
+function UsersPage() {
+  const users = useSelector((state) => state.usersPage.users);
+  const pageSize = useSelector((state) => state.usersPage.pageSize);
+  const totalUsers = useSelector((state) => state.usersPage.totalUsers);
+  const isFetching = useSelector((state) => state.usersPage.isFetching);
+  const followingInProgress = useSelector(
+    (state) => state.usersPage.followingInProgress
+  );
+
+  useEffect(() => {
+    document.title = 'Друзья';
+  }, []);
+
+  const dispatch = useDispatch();
+  const curPage = Number(useParams().page);
+  useEffect(() => {
+    if (curPage) {
+      dispatch(requestUsers(curPage, pageSize, totalUsers));
+    }
+  }, [curPage]);
+
+  // если NaN, например url был users/24lol
+  if (!curPage) {
+    return <Redirect to="/users/1" />;
+  }
 
   return (
     <div id="users">
@@ -34,7 +38,19 @@ function UsersPage(props) {
       {isFetching ? <Loader /> : null}
       {/* TODO: заменить undefined на '' */}
       <div className={`wrapper ${isFetching ? 'isFetching' : undefined}`}>
-        {users}
+        {users.map((user) => (
+          <User
+            key={user.id}
+            id={user.id}
+            avatar={user.photos.small}
+            name={user.name}
+            status={user.status}
+            followed={user.followed}
+            follow={(id) => dispatch(follow(id))}
+            unfollow={(id) => dispatch(unfollow(id))}
+            followingInProgress={followingInProgress}
+          />
+        ))}
         <Paginator
           curPage={curPage}
           totalUsers={totalUsers}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
 export { default as AuthPage } from './AuthPage/AuthPage';
 export { default as DialogsPage } from './DialogsPage/DialogsPage';
 export { default as ProfilePage } from './ProfilePage/ProfilePage';
-export { default as UsersPage } from './UsersPage/UsersContainer';
+export { default as UsersPage } from './UsersPage/UsersPage';
